2463c81c0a50eddd8b4d87119be174cfb288d703
someonekong <someonekong3001@gmail.com>
2024-07-25 13:40:48 +0800

    update OpenRouter_Client

--- client_impl/openrouter_impl.py
+++ client_impl/openrouter_impl.py
@@ -24,6 +24,18 @@ class OpenRouter_Client(OpenAI_Client):
             api_key=api_key,
         )
 
+    async def chat_stream_async(self, model_name, history, model_param, client_param):
+        model_param_temp = model_param.copy()
+        model_name_temp = model_name
+        if '|' in model_name:
+            chunks = model_name.split('|')
+            model_name_temp = chunks[0]
+            provider = chunks[1]
+            model_param_temp['provider'] = {'order': [provider]}
+
+        async for chunk in super().chat_stream_async(model_name_temp, history, model_param_temp, client_param):
+            yield chunk
+
 
 if __name__ == '__main__':
     import asyncio
